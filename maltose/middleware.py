288fe412f7b0a8cd9a43d66205a8b083539040a9
AberSheeran <AberSheeran@qq.com>
2019-04-05 16:29:19 +0800

    Fix testserve

--- maltose/middleware.py
+++ maltose/middleware.py
@@ -6,5 +6,5 @@ from django.conf import settings
 class RequestHostMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
-        if settings.DEBUG and request.get_host() == "testserver":
+        if request.get_host() == "testserver":
             request.META['HTTP_HOST'] = settings.HOMEPAGE.split("://")[1].split("/")[0]
