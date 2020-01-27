5ef83378abe5a1caf77fac40b9d2fc48c9d45839
abersheeran <me@abersheeran.com>
2020-01-27 21:21:53 +0800

    update editoer

---
+++ maltose/editor/precache-manifest.d395f1e34c62bf286a04b8c7842feaf9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b02699ca4930bdd536abbec29e7fbd9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e1fb82c9c5185b786308",
+    "url": "./static/css/2.bd501cb7.chunk.css"
+  },
+  {
+    "revision": "6074c05e81e1fe358d27",
+    "url": "./static/css/main.2e8b5f4b.chunk.css"
+  },
+  {
+    "revision": "e1fb82c9c5185b786308",
+    "url": "./static/js/2.fbde52f6.chunk.js"
+  },
+  {
+    "revision": "ebd300618999f6b962cbb6603530943f",
+    "url": "./static/js/2.fbde52f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d615273b78b14640831",
+    "url": "./static/js/3.2811850f.chunk.js"
+  },
+  {
+    "revision": "6074c05e81e1fe358d27",
+    "url": "./static/js/main.f4174e80.chunk.js"
+  },
+  {
+    "revision": "86208216942f049a1ff1",
+    "url": "./static/js/runtime-main.4b3c3e35.js"
+  }
+]);
